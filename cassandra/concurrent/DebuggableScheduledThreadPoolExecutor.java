7109b2c88617fde332546abfee8f11fd5a3148eb
Chen Shang <shangcs@yahoo.com>
2015-12-07 09:05:22 -0800

    upd

--- cassandra/concurrent/DebuggableScheduledThreadPoolExecutor.java
+++ cassandra/concurrent/DebuggableScheduledThreadPoolExecutor.java
@@ -6,18 +6,12 @@
  */
 public final class DebuggableScheduledThreadPoolExecutor extends ScheduledThreadPoolExecutor
 {
-    private static Logger logger_ = Logger.getLogger(DebuggableScheduledThreadPoolExecutor.class);
-    
     public DebuggableScheduledThreadPoolExecutor(int threads,
             ThreadFactory threadFactory)
     {
         super(threads, threadFactory);        
     }
     
-    /**
-     *  (non-Javadoc)
-     * @see java.util.concurrent.ThreadPoolExecutor#afterExecute(java.lang.Runnable, java.lang.Throwable)
-     */
     public void afterExecute(Runnable r, Throwable t)
     {
         super.afterExecute(r,t);
